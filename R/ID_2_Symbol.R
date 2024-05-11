8169eb6dca2a327ee47bd6f6868e7ce75c1ad1da
Cal_luo <carl001226@foxmail.com>
2024-05-11 15:58:10 +0800

    ci: run usethis::use_github_action("lint") to setup lintr in Github Action

--- R/ID_2_Symbol.R
+++ R/ID_2_Symbol.R
@@ -7,12 +7,11 @@
 #'
 #' @examples
 #' library(ComprehensivePipelineUtils)
-#' ID_2_Symbol(c('ENSG00000162444', 'ENSG00000130939'))
+#' ID_2_Symbol(c("ENSG00000162444", "ENSG00000130939"))
 ID_2_Symbol <- function(ID_list) {
-  load('data/symbol_ID_pairs_based_gencodeV22.rda')
+  load("data/symbol_ID_pairs_based_gencodeV22.rda")
   res <- unlist(lapply(ID_list, function(x) {
     return(subset(symbol_ID_pairs_based_gencodeV22, gene_id == x)$gene_name)
   }))
   return(res)
 }
-
